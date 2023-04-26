b0766cef6d4fa1f90a6c2136bcbee1a75904d3d4
Irvine99 <60505161+Irvine99@users.noreply.github.com>
2023-04-26 15:04:09 +0200

    fix

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-    <!--Tailwind -->
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-      <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
-      }
+require 'src/controller/homeController.php';
+if(isset($_GET['action'])){
+        switch($_GET['action']) {
+            case 'test':
+                test();
+                break;
+        } 
     }
-  </script>
-  <!--TailwindElement -->
-  <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></script>
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+test();
