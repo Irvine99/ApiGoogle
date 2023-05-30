129acea5d99ad1d90f4949e10419bcfdfd0098e0
Load3rR <122812536+Load3rR@users.noreply.github.com>
2023-05-30 11:24:24 +0100

    benmaj

---
+++ src/include/footer.php
@@ -0,0 +1,12 @@
+<footer class="grid bg-gray-800 place-content-around">
+    
+    <div class="flex p-2 justify-center mb-2     ">
+        <img class="w-[32%] object-contain justify-center" src="assets/img/logo_transpa.png"></img>
+    </div>
+
+    <div class="">
+    <a href="#" class="flex text-white items-center p-2 justify-center"></a>
+    <p class="text-white p-2">Copyright Maîtrise Web 2023</p>
+    </div>
+
+</footer>
