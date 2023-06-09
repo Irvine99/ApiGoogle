aba57b9bbefe3f19ad9d04dc062e97e72482e431
Tom <tom@hotmail.com>
2023-06-09 08:21:45 +0200

    bonbon

--- src/view/admin.php
+++ src/view/admin.php
@@ -61,64 +61,7 @@ require_once 'src/model/Project.php';
                     <div class="overflow-hidden">
 
                         <table class="min-w-full text-left text-sm font-light">
-                            <!-- <thead class="border-b font-medium dark:border-neutral-500">
                             
-                                    
-                                <tr>
-                                    <th scope="col" class="px-6 py-4">id</th>
-                                    <th scope="col" class="px-6 py-4">prénom</th>
-                                    <th scope="col" class="px-6 py-4">nom</th>
-                                    <th scope="col" class="px-6 py-4">email</th>
-                                    <th scope="col" class="px-6 py-4">nom du projet</th>
-                                    <th scope="col" class="px-6 py-4">json</th>
-                                    <th scope="col" class="px-6 py-4">logo</th>
-                                    <th scope="col" class="px-6 py-4 text-center">Actions</th>
-                                </tr>
-                                
-                            </thead> -->
-                            <!-- <?php if (is_array($allUsers)) {
-                             foreach ($allUsers as $user)  {?>
-                            <tbody>
-                                <tr class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-                                    <td class="whitespace-nowrap px-6 py-4 font-medium"><?php echo $user->id ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4 font-medium"><?php echo $user->username ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4"><?php echo $user->userlastname ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4"><?php echo $user->email ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4"><?php echo $user->name ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4"><?php echo $user->json ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4"><?php echo $user->logo ?></td>
-                                    <td class="whitespace-nowrap px-6 py-4 text-center">
-                                        
-                                        
-                                        
-                                        
-                                        <form method="POST" action="?action=deleteUser">
-                                        <input type="hidden" name="projectId" value="<?php echo $user->id_pro ?>">
-                                        <input type="hidden" name="userId" value="<?php echo $user->id ?>">
-                                        <button class="mb-2 bg-red-500 rounded-lg py-1 px-2 text-white hover:bg-red-800 w-[100px]  " type="submit">Supprimer</button>
-                                        </form>
-                                        
-                                        
-                            
-                                        <form method="POST" action="?action=modifPage">
-                                        <input type="hidden" name="username" value="<?php echo $user->username ?>">
-                                        <input type="hidden" name="userlastname" value="<?php echo $user->userlastname ?>">
-                                        <input type="hidden" name="useremail" value="<?php echo $user->email ?>">
-                                        <input type="hidden" name="projectName" value="<?php echo $user->name ?>">
-                                        <input type="hidden" name="projectJson" value="<?php echo $user->json ?>">
-                                        <input type="hidden" name="projectLogo" value="<?php echo $user->logo ?>">
-                                        <input type="hidden" name="projectId" value="<?php echo $user->id_pro ?>">
-                                        <input type="hidden" name="userId" value="<?php echo $user->id ?>">
-                                        <button class="bg-blue-500 rounded-lg py-1 px-2 w-[100px] text-white hover:bg-blue-800" type="submit">Modifier</button>
-                                        </form>
-
-                                        
-
-                                       
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <?php } } ?> -->
                             <div class="container">
     <table class="table">
       <thead>
